ae49b91845feeca552f6cb70c264bfd7fd155cbb
Yash Karande <yash.karande@sourcefuse.com>
2022-09-25 16:28:44 +0530

    __tests__: unit: Uncomment tests.

--- src/__tests__/unit/user-rest-consumer.controller.unit.ts
+++ src/__tests__/unit/user-rest-consumer.controller.unit.ts
@@ -1,53 +1,62 @@
-// describe('User Rest Consumer (Unit)', () => {
-//   let repo: StubbedInstanceWithSinonAccessor<UserNewRepository>;
-//   beforeEach(givenStubbedRepo);
-//   it('creates a new user', async () => {
-//     const user = new UserNew({
-//       id: 1,
-//       name: 'ABC',
-//       role_id: 1,
-//     });
-//     repo.stubs.create.resolves(user);
-//     let controller = new UserNewControllerController(repo);
-//     const response = await controller.create(user);
-//     expect(response).to.containEql(user);
-//   });
+import {
+  createStubInstance,
+  expect,
+  StubbedInstanceWithSinonAccessor,
+} from '@loopback/testlab';
+import {UserNewControllerController} from '../../controllers';
+import {UserNew} from '../../models';
+import {UserNewRepository} from '../../repositories';
 
-//   it('gets all the users', async () => {
-//     const users = [
-//       new UserNew({id: 2, name: 'Get1', role_id: 2}),
-//       new UserNew({id: 3, name: 'Get2', role_id: 2}),
-//     ];
-//     repo.stubs.find.resolves(users);
-//     let controller = new UserNewControllerController(repo);
-//     const response = await controller.find();
-//     expect(response).to.eql(users);
-//   });
+describe('User Rest Consumer (Unit)', () => {
+  let repo: StubbedInstanceWithSinonAccessor<UserNewRepository>;
+  beforeEach(givenStubbedRepo);
+  it('creates a new user', async () => {
+    const user = new UserNew({
+      id: 1,
+      name: 'ABC',
+      role_id: 1,
+    });
+    repo.stubs.create.resolves(user);
+    let controller = new UserNewControllerController(repo);
+    const response = await controller.create(user);
+    expect(response).to.containEql(user);
+  });
 
-//   it('gets a specific user', async () => {
-//     const user = new UserNew({id: 4, name: 'Get3', role_id: 1});
-//     repo.stubs.findById.resolves(user);
-//     let controller = new UserNewControllerController(repo);
-//     const response = await controller.findById(4);
-//     expect(response).to.containEql(user);
-//   });
+  it('gets all the users', async () => {
+    const users = [
+      new UserNew({id: 2, name: 'Get1', role_id: 2}),
+      new UserNew({id: 3, name: 'Get2', role_id: 2}),
+    ];
+    repo.stubs.find.resolves(users);
+    let controller = new UserNewControllerController(repo);
+    const response = await controller.find();
+    expect(response).to.eql(users);
+  });
 
-//   it('updates a specific user', async () => {
-//     const user = new UserNew({id: 5, name: 'Update', role_id: 1});
-//     repo.stubs.replaceById.resolves();
-//     let controller = new UserNewControllerController(repo);
-//     const response = await controller.replaceById(5, user);
-//     expect(response).to.eql(undefined);
-//   });
+  it('gets a specific user', async () => {
+    const user = new UserNew({id: 4, name: 'Get3', role_id: 1});
+    repo.stubs.findById.resolves(user);
+    let controller = new UserNewControllerController(repo);
+    const response = await controller.findById(4);
+    expect(response).to.containEql(user);
+  });
 
-//   it('deletes a specific user', async () => {
-//     repo.stubs.deleteById.resolves();
-//     let controller = new UserNewControllerController(repo);
-//     const response = await controller.deleteById(6);
-//     expect(response).to.eql(undefined);
-//   });
+  it('updates a specific user', async () => {
+    const user = new UserNew({id: 5, name: 'Update', role_id: 1});
+    repo.stubs.replaceById.resolves();
+    let controller = new UserNewControllerController(repo);
+    const response = await controller.replaceById(5, user);
+    expect(response).to.eql(undefined);
+  });
 
-//   function givenStubbedRepo() {
-//     repo = createStubInstance(UserNewRepository);
-//   }
-// });
+  it('deletes a specific user', async () => {
+    repo.stubs.deleteById.resolves();
+    let controller = new UserNewControllerController(repo);
+    const response = await controller.deleteById(6);
+    expect(response).to.eql(undefined);
+  });
+
+  function givenStubbedRepo() {
+    repo = createStubInstance(UserNewRepository);
+  }
+});
